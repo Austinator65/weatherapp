ab005989b056b10633d4b60bac99402d0cfeb4e3
Austinator65 <atisomofokeng167@gmail.com>
2023-11-14 16:21:11 +0200

    Created a Basic Weather Application App

--- src/main/java/com/austin/weatherapp/controller/WeatherService.java
+++ src/main/java/com/austin/weatherapp/controller/WeatherService.java
@@ -15,13 +15,14 @@ import okhttp3.Response;
 public class WeatherService {
     private OkHttpClient client;
     private Response response;
+    private String unit;
 
-    public JSONObject getWeather(String name) {
+    public JSONObject getWeatherObject(String name) {
         client = new OkHttpClient();
 
         Request request = new Request.Builder()
                 .url("https://api.openweathermap.org/data/2.5/weather?q=" + name
-                        + "&units=metric&appid=6bed260650ab58e2e87160501c177ba8")
+                        + "&units="+getUnit()+"&appid=6bed260650ab58e2e87160501c177ba8")
                 .build();
 
         try {
@@ -38,10 +39,38 @@ public class WeatherService {
 
     }
 
-    private JSONArray weatherArray(String name) throws JSONException {
+    public JSONArray getWeatherArray(String name) throws JSONException {
 
-        JSONArray weatherJSONArray = getWeather(name).getJSONArray("weather");
+        JSONArray weatherJSONArray = getWeatherObject(name).getJSONArray("weather");
 
         return weatherJSONArray;
+    }
+
+    public JSONObject getMainObject(String name) throws JSONException {
+        JSONObject mainObject = getWeatherObject(name).getJSONObject("main");
+        return mainObject;
+    }
+
+    public JSONObject getWindObjewct(String name) throws JSONException {
+        JSONObject windObject = getWeatherObject(name).getJSONObject("wind");
+        return windObject;
+    }
+
+    public JSONObject getCloudObject(String name) throws JSONException {
+        JSONObject cloudObject = getWeatherObject(name).getJSONObject("cloud");
+        return cloudObject;
+    }
+
+    public JSONObject getSun(String name) throws JSONException {
+        JSONObject sunObject = getWeatherObject(name).getJSONObject("sys");
+        return sunObject;
+    }
+
+    public void setUnit(String unit){
+        this.unit = unit;
+    }
+
+    public String getUnit(){
+        return unit;
     }
 }
